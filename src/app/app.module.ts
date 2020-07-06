f27016db04e98d67ba6f3e6040523506e7330fc0
Amodh Shenoy <amodhshenoy@gmail.com>
2020-07-07 05:07:17 +0530

    Assignment 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {FlexLayoutModule} from '@angular/flex-layout';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatListModule } from '@angular/material/list';
 // import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatCardModule} from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
@@ -19,9 +19,10 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 
-import {DishService} from './services/dish.service';
-import {PromotionService} from './services/promotion.service';
-import {AppRoutingModule} from './app-routing/app-routing.module';
+import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
+import { LeaderService } from'./services/leader.service';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
@@ -47,7 +48,8 @@ import {AppRoutingModule} from './app-routing/app-routing.module';
   ],
   providers: [
     DishService,
-    PromotionService
+    PromotionService,
+    LeaderService
   ],
   bootstrap: [AppComponent]
 })
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Dish } from '../shared/dish';
-import {DishService} from '../services/dish.service';
-import {Promotion} from '../shared/promotion';
-import {PromotionService} from '../services/promotion.service';
+import { DishService } from '../services/dish.service';
+import { Promotion } from '../shared/promotion';
+import { PromotionService } from '../services/promotion.service';
+import { Leader } from '../shared/leader';
+import { LeaderService } from  '../services/leader.service';
 
 @Component({
   selector: 'app-home',
@@ -12,13 +14,16 @@ import {PromotionService} from '../services/promotion.service';
 export class HomeComponent implements OnInit {
   dish :Dish;
   promotion: Promotion;
+  leader: Leader;
   
   constructor(private dishService: DishService,
-    private promotionService: PromotionService) { }
+    private promotionService: PromotionService,
+    private leaderService: LeaderService) { }
 
   ngOnInit(){
     this.dish = this.dishService.getFeaturedDish();
     this.promotion = this.promotionService.getFeaturedPromotion();
+    this.leader = this.leaderService.getFeaturedLeader();
   }
 
 }
